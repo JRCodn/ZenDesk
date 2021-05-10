c62c80af307f30cb38875e73e1ee9ca51b4bef57
Jay Reidy <jaylreidy21@gmail.com>
2021-05-10 20:33:05 +1000

    changed env

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
-require_relative 'controller/controller'
-require_relative 'router'
+# require_relative 'controller/controller'
+# require_relative 'router'
+require 'APIKEY'
 
-klass = Controller.new
-router = Router.new(klass)
-router.run
+# klass = Controller.new
+# router = Router.new(klass)
+# router.run
+ puts APIKEY
